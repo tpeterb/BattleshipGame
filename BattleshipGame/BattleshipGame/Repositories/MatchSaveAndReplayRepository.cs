8354026e007e7c4b5b5764800b1852ecf8dac6a4
NSdeIK <71137188+NSdeIK@users.noreply.github.com>
2022-12-17 14:19:39 +0100

    Repository bővítése #2

--- BattleshipGame/BattleshipGame/Repositories/MatchSaveAndReplayRepository.cs
+++ BattleshipGame/BattleshipGame/Repositories/MatchSaveAndReplayRepository.cs
@@ -9,5 +9,36 @@ namespace BattleshipGame.Repositories
 {
     public static class MatchSaveAndReplayRepository
     {
+        public static IList<MatchSaveAndReplay> GetMatchSaveAndReplays()
+        {
+            var database = new DatabaseContext();
+
+            using (database)
+            {
+                return database.matchSaveAndReplays.ToList();
+            }
+        }
+
+        public static MatchSaveAndReplay GetMatchId(long id)
+        {
+            var database = new DatabaseContext();
+
+            using (database)
+            {
+                return database.matchSaveAndReplays.Where(r => r.Id == id).FirstOrDefault();
+            }
+        }
+
+        public static void StoreMatchSaveAndReplay(MatchSaveAndReplay match)
+        {
+            var database = new DatabaseContext();
+
+            using (database)
+            {
+                database.matchSaveAndReplays.Add(match);
+
+                database.SaveChanges();
+            }
+        }
     }
 }
