766c66562897e6d98bb9314602e517a99d812103
NSdeIK <71137188+NSdeIK@users.noreply.github.com>
2022-12-17 11:27:33 +0100

    Adatbázis létrehozása #1

---
+++ BattleshipGame/BattleshipGame/Repositories/MatchSaveAndReplayRepository.cs
@@ -0,0 +1,13 @@
+﻿using BattleshipGame.Repositories.Contexts;
+using BattleshipGame.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleshipGame.Repositories
+{
+    public static class MatchSaveAndReplayRepository
+    {
+    }
+}
