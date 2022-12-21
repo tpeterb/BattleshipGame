39f233d83113b8aaab59e1ecc62541b99a22fc45
Armisam <csabaszekely99@gmail.com>
2022-12-21 16:25:50 +0100

    Frontend létrehozása #3 - Játékba belépés UI-al

--- BattleshipGame/BattleshipGame/View/MainWindow.xaml.cs
+++ BattleshipGame/BattleshipGame/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Sytem imports
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BattleshipGame
+// Declared class imports
+using BattleshipGame.Model;
+
+namespace BattleshipGame.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -52,6 +56,72 @@ namespace BattleshipGame
         {
             LoadMenuGrid.Visibility = Visibility.Hidden;
             MainMenuGrid.Visibility = Visibility.Visible;
+        }
+
+        private void onClickNewGame(object sender, RoutedEventArgs e)
+        {
+            MainMenuGrid.Visibility = Visibility.Hidden;
+            NewGameMenuGrid.Visibility = Visibility.Visible;
+        }
+
+        private void onClickBackNewGame(object sender, RoutedEventArgs e)
+        {
+            NewGameMenuGrid.Visibility = Visibility.Hidden;
+            MainMenuGrid.Visibility = Visibility.Visible;
+
+            OnePlayerModeButton.Visibility = Visibility.Visible;
+            TwoPlayerModeButton.Visibility = Visibility.Visible;
+
+            Player1Label.Visibility = Visibility.Hidden;
+            Player1TextBox.Visibility = Visibility.Hidden;
+            Player2Label.Visibility = Visibility.Hidden;
+            Player2TextBox.Visibility = Visibility.Hidden;
+
+            StartGameButton.Visibility = Visibility.Hidden;
+        }
+
+        private void onClickOnePlayerMode(object sender, RoutedEventArgs e)
+        {
+            OnePlayerModeButton.Visibility = Visibility.Hidden;
+            TwoPlayerModeButton.Visibility = Visibility.Hidden;
+
+            Player1Label.Visibility = Visibility.Visible;
+            Player1TextBox.Visibility = Visibility.Visible;
+
+            StartGameButton.Visibility = Visibility.Visible;
+        }
+
+        private void onClickTwoPlayerMode(object sender, RoutedEventArgs e)
+        {
+            OnePlayerModeButton.Visibility = Visibility.Hidden;
+            TwoPlayerModeButton.Visibility = Visibility.Hidden;
+
+            Player1Label.Visibility = Visibility.Visible;
+            Player1TextBox.Visibility = Visibility.Visible;
+
+            Player2Label.Visibility = Visibility.Visible;
+            Player2TextBox.Visibility = Visibility.Visible;
+
+            StartGameButton.Visibility = Visibility.Visible;
+        }
+
+        private void onClickStartGame(object sender, RoutedEventArgs e)
+        {
+            OnePlayerModeButton.Visibility = Visibility.Hidden;
+            TwoPlayerModeButton.Visibility = Visibility.Hidden;
+
+            Player1Label.Visibility = Visibility.Visible;
+            Player1TextBox.Visibility = Visibility.Visible;
+
+            Player2Label.Visibility = Visibility.Visible;
+            Player2TextBox.Visibility = Visibility.Visible;
+
+            StartGameButton.Visibility = Visibility.Visible;
+
+
+
+            NewGameMenuGrid.Visibility = Visibility.Hidden;
+            GameGrid.Visibility = Visibility.Visible;
         }
     }
 }
